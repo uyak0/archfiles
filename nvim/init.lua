bda8aeecb894722e1c0488cb851f5992e479a0e6
uyak0 <milkycoffeethings@gmail.com>
2024-03-06 00:27:58 +0800

    surround.vim

--- nvim/init.lua
+++ nvim/init.lua
@@ -29,6 +29,10 @@ vim.opt.rtp:prepend(lazypath)
 
 -- [[ Configure plugins ]]
 require('lazy').setup({
+
+  -- Surround.vim
+  'tpope/vim-surround',
+
   -- Colorizer
   {
     'NvChad/nvim-colorizer.lua',
@@ -44,6 +48,7 @@ require('lazy').setup({
       }
     end,
   },
+
   -- Auto pair
   {
     'windwp/nvim-autopairs',
@@ -69,7 +74,7 @@ require('lazy').setup({
   -- Kanagawa Theme
   'rebelot/kanagawa.nvim',
   
-  {
+  { -- NvimTree 
     "nvim-tree/nvim-tree.lua",
     dependencies = {
       "nvim-tree/nvim-web-devicons",
