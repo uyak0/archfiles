ab2088f2db5ea64f1dfa3cbcf3979a9827519cad
uyak0 <milkycoffeethings@gmail.com>
2024-04-16 22:00:25 +0800

    deleted init.lua in plugin dir

--- nvim/init.lua
+++ nvim/init.lua
@@ -36,7 +36,22 @@ vim.opt.rtp:prepend(lazypath)
 -- [[ Configure plugins ]]
 require('lazy').setup({
 
-  require('init'),
+  require('plugins.markdown'),
+  require('plugins.colorizer'),
+  require('plugins.themes'),
+  require('plugins.nvim-tree'),
+  require('plugins.laravel-nvim'),
+  require('plugins.lualine'),
+  require('plugins.no-setup-plugins'),
+  require('plugins.lspconfig'),
+  require('plugins.nvim-cmp'),
+  require('plugins.nvim-telescope'),
+  require('plugins.indent-blankline'),
+  require('plugins.gitsigns'),
+  require('plugins.nvim-treesitter'),
+  require('plugins.copilot'),
+  require('plugins.autotag'),
+  require('keybindings'),
 
 }, {})
 
