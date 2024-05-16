de10826419ddfe3999742e547257a1a189950c6f
uyak0 <milkycoffeethings@gmail.com>
2024-05-16 15:37:53 +0800

    update: configuration-pc.nix
    Cleaned the code

--- configuration-pc.nix
+++ configuration-pc.nix
@@ -1,9 +1,13 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, lib, pkgs, ... }:
-  
+
+# Adding unstable packages
+let 
+  unstable = import <nixos-unstable> {
+    config = {
+      allowUnfree = true;
+    };
+  };
+in
 {
   imports = 
   [ # Include the results of the hardware scan.
@@ -11,46 +15,30 @@
     <home-manager/nixos>
   ];
 
-  # Settings for Nvidia
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-  };
+  i18n = {
+    # Select internationalisation properties.
+    defaultLocale = "en_US.UTF-8";
 
-  # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["nvidia"];
+    extraLocaleSettings = {
+      LC_ADDRESS = "ms_MY.UTF-8";
+      LC_IDENTIFICATION = "ms_MY.UTF-8";
+      LC_MEASUREMENT = "ms_MY.UTF-8";
+      LC_MONETARY = "ms_MY.UTF-8";
+      LC_NAME = "ms_MY.UTF-8";
+      LC_NUMERIC = "ms_MY.UTF-8";
+      LC_PAPER = "ms_MY.UTF-8";
+      LC_TELEPHONE = "ms_MY.UTF-8";
+      LC_TIME = "en_US.UTF-8";
+    };
 
-  hardware.nvidia = {
-
-    # Modesetting is required.
-    modesetting.enable = true;
-
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
-    # of just the bare essentials.
-    powerManagement.enable = false;
-
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at: 
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-    # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = false;
-
-    # Enable the Nvidia settings menu,
-	# accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    inputMethod = {
+      enabled = "fcitx5";
+      fcitx5.addons = with pkgs; [
+	fcitx5-chinese-addons
+	fcitx5-rime
+	fcitx5-gtk
+      ];
+    };
   };
 
   # Hyprland
@@ -59,8 +47,16 @@
     enable = true;
     # Whether to enable XWayland
     xwayland.enable = true;
+    # https://wiki.hyprland.org/0.32.1/Nix/Options-Overrides/#nvidia-patches
+    enableNvidiaPatches = true; 
   }; 
+  security.polkit.enable = true;
 
+  # Enable the KDE Connect service
+  programs.kdeconnect = {
+    package = pkgs.libsForQt5.kdeconnect-kde;
+    enable = true;
+  };
 
   # Home Manager
   home-manager.users.uyako= {
@@ -75,21 +71,21 @@
 
       oh-my-zsh = {
 	enable = true;
-	theme = "wedisagree";
+	theme = "neo";
 	plugins = [
 	  "git"
 	  "zoxide"
 	];
+	custom = "$HOME/.config/zsh";
       };
 
       shellAliases = {
 	nixconf = "sudo -E nvim /etc/nixos/configuration.nix";
 	nixbuild = "sudo nixos-rebuild switch";
 	vi = "nvim";
-	vuedev = "cd ~/Projects/e-quiz-web-app/vue-frontend && npm i && npm run dev";
-	phpserve = "cd ~/Projects/e-quiz-web-app/laravel-backend && php artisan serve";
 	ls = "eza";
 	lg = "lazygit";
+	hyprconf = "nvim ~/.config/hypr/hyprland.conf";
       };
     };
 
@@ -113,9 +109,6 @@
     ];
   };
 
-  # Enabling MariaDB
-  services.mysql.package = pkgs.mariadb;
-  services.mysql.enable = true;
 
   # Allow unfree software
   nixpkgs.config.allowUnfree = true;
@@ -124,8 +117,7 @@
   programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
 
-  services.xserver.displayManager.gdm.wayland = true;  
-  
+  # Ozone Flags for Wayland
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
   # cursor size
   environment.variables.XCURSOR_SIZE = "32";
@@ -147,53 +139,9 @@
   # Set your time zone.
   time.timeZone = "Asia/Kuala_Lumpur";
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "ms_MY.UTF-8";
-    LC_IDENTIFICATION = "ms_MY.UTF-8";
-    LC_MEASUREMENT = "ms_MY.UTF-8";
-    LC_MONETARY = "ms_MY.UTF-8";
-    LC_NAME = "ms_MY.UTF-8";
-    LC_NUMERIC = "ms_MY.UTF-8";
-    LC_PAPER = "ms_MY.UTF-8";
-    LC_TELEPHONE = "ms_MY.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "us";
-    xkbVariant = "";
-  };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
+  # Enable sound 
   sound.enable = true;
-  hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
@@ -212,36 +160,171 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-	discord
-	(nerdfonts.override { fonts = [
-	  "FiraCode"
-	  "JetBrainsMono"
-	]; })
-	vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-	wget
-	neofetch
-	brave
-	neovim
-	git
-	hyprland
-	btop
-	lazygit
-	libgccjit
-	libgcc
-	binutils
-	gcc_multi
-	nodePackages_latest.nodejs
-	php
-	php83Packages.composer
-	waybar
-	hyprpaper
-	unzip
-	gnome.adwaita-icon-theme
-	clang-tools
-	kitty
-	rofi
-	spotify
+
+    # Fonts
+    (nerdfonts.override { fonts = [
+      "FiraCode"
+      "JetBrainsMono"
+    ]; })
+
+
+    # Hyprland Essentials
+    waybar
+    hyprpaper
+    gnome.adwaita-icon-theme
+
+    # Apps
+    unstable.obsidian
+    discord
+    brave
+    spotify
+    droidcam
+    kitty
+    variety
+
+    # Development
+    git
+    lazygit
+    libgccjit
+    libgcc
+    binutils
+    gcc_multi
+    nodePackages_latest.nodejs
+    php
+    php83Packages.composer
+    clang-tools
+    python3
+    wget
+
+    vim 
+    neovim
+
+    # Screen Sharing
+    xdg-desktop-portal-wlr
+    unstable.xwaylandvideobridge
+
+    # Utilities
+    neofetch
+    btop
+    unzip
+    grim
+    slurp
+    wl-clipboard
+    playerctl
+    pavucontrol
+    rofi
+    swaynotificationcenter
+    libsForQt5.kdeconnect-kde
+
+    linuxKernel.packages.linux_libre.v4l2loopback
   ];
+
+  # Virtual Camera 
+  boot.extraModulePackages = with config.boot.kernelPackages;
+  [ v4l2loopback.out ];
+  boot.kernelModules = [
+    "v4l2loopback"
+  ];
+  boot.extraModprobeConfig = ''
+    options v4l2loopback exclusive_caps=1 card_label="Virtual Camera"
+  '';
+
+  # List of services
+  services = {
+    xserver = {
+      # Enable the X11 windowing system.
+      enable = true;
+
+      # Configure keymap in X11
+      layout = "us";
+      xkbVariant = "";
+
+      # Load nvidia driver for Xorg and Wayland
+      videoDrivers = ["nvidia"];
+
+      displayManager.gdm.enable = true;
+      displayManager.gdm.wayland = true;  
+
+      # Enable the GNOME Desktop Environment.
+      desktopManager.gnome.enable = true;
+    };
+
+    # Enabling MariaDB
+    mysql.package = pkgs.mariadb;
+    mysql.enable = true;
+
+    # Enable CUPS to print documents.
+    printing.enable = true;
+
+    # Enable sound with pipewire
+    pipewire = {
+      enable = true;
+      wireplumber.enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      # If you want to use JACK applications, uncomment this
+      #jack.enable = true;
+
+      # use the example session manager (no others are packaged yet so this is enabled by default,
+      # no need to redefine it in your config for now)
+      #media-session.enable = true;
+    };
+  };
+
+  # Hardware Configs
+  hardware = {
+    # Disable PulseAudio in favor of WirePlumber
+    pulseaudio.enable = false;
+
+    # Settings for Nvidia
+    opengl = {
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
+    };
+
+    nvidia = {
+
+      # Modesetting is required.
+      modesetting.enable = true;
+
+      # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+      # Enable this if you have graphical corruption issues or application crashes after waking
+      # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+      # of just the bare essentials.
+      powerManagement.enable = false;
+
+      # Fine-grained power management. Turns off GPU when not in use.
+      # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+      powerManagement.finegrained = false;
+
+      # Use the NVidia open source kernel module (not to be confused with the
+      # independent third-party "nouveau" open source driver).
+      # Support is limited to the Turing and later architectures. Full list of 
+      # supported GPUs is at: 
+      # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+      # Only available from driver 515.43.04+
+      # Currently alpha-quality/buggy, so false is currently the recommended setting.
+      open = false;
+
+      # Enable the Nvidia settings menu,
+      # accessible via `nvidia-settings`.
+      nvidiaSettings = true;
+
+      # Optionally, you may need to select the appropriate driver version for your specific GPU.
+      package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+	version = "525.116.04";
+	sha256_64bit = "sha256-hhDsgkR8/3LLXxizZX7ppjSlFRZiuK2QHrgfTE+2F/4=";
+	sha256_aarch64 = "sha256-k7k22z5PYZdBVfuYXVcl9SFUMqZmK4qyxoRwlYyRdgU=";
+	openSha256 = "sha256-dktHCoESqoNfu5M73aY5MQGROlZawZwzBqs3RkOyfoQ=";
+	settingsSha256 = "sha256-qNjfsT9NGV151EHnG4fgBonVFSKc4yFEVomtXg9uYD4=";
+	persistencedSha256 = "sha256-ci86XGlno6DbHw6rkVSzBpopaapfJvk0+lHcR4LDq50=";
+
+	ibtSupport = true;
+      };  
+    };
+  };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
