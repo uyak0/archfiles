690688540fa7fee40f41ced8268484e4a0a31fb5
uyak0 <milkycoffeethings@gmail.com>
2024-05-19 23:11:24 +0800

    update: nixpkgs/config.nix

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -1,24 +1,38 @@
 {
-  allowUnfree = true;
-  packageOverrides = pkgs: with pkgs; {
-    myPackages = pkgs.buildEnv {
-      name = "my-packages";
-      paths = [
-	rustup
-	jdt-language-server
-      	nodejs_21
-	php
-	neovim
-	git
-	php83Packages.composer
-	ripgrep
-	ripgrep-all
-	clang
-	zulu
-	libclang
-	opencv
-      ];
-      pathsToLink = [ "/share" "/bin" ];
-    };
-  };
+	allowUnfree = true;
+	packageOverrides = pkgs: with pkgs; {
+		devPackages = pkgs.buildEnv {
+			name = "dev-packages";
+			paths = [
+				git
+				lazygit
+				libgcc
+				binutils
+				nodePackages_latest.nodejs
+				php
+				php83Packages.composer
+				clang-tools
+				python3
+				zulu
+				wget
+				ripgrep
+				rustup
+			];
+			pathsToLink = [ "/share" "/bin" ];
+		};
+
+		# LSP
+		lspPackages = pkgs.buildEnv {
+			name = "lsp-packages";
+			paths = [
+				jdt-language-server
+				nil
+				lua-language-server
+				phpactor
+				nodePackages_latest.volar
+				nodePackages_latest.typescript-language-server
+			];
+			pathsToLink = [ "/share" "/bin" ];
+		};
+	};
 }
