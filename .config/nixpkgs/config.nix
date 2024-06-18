b3ddd71f7a5492fc92e57162fd6026c95e5c44fd
uyak0 <milkycoffeethings@gmail.com>
2024-06-18 09:56:47 +0800

    update: nixpkgs

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -1,16 +1,25 @@
+let
+	phpPackages = import (builtins.fetchTarball {
+		url = "https://github.com/NixOS/nixpkgs/archive/4426104c8c900fbe048c33a0e6f68a006235ac50.tar.gz";
+	}) {};
+
+	php74 = phpPackages.php74;
+	php74Extensions = phpPackages.php74Extensions;
+in
 {
 	allowUnfree = true;
 	packageOverrides = pkgs: with pkgs; {
 		devPackages = pkgs.buildEnv {
 			name = "dev-packages";
 			paths = [
+				php74
+				php74Extensions.mbstring
+
+
 				git
 				lazygit
 				libgcc
 				binutils
-				nodePackages_latest.nodejs
-				php
-				php83Packages.composer
 				clang-tools
 				python3
 				zulu
